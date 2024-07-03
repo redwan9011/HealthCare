163be66e04ff8ffa5615759def58bc8d86b68146
redwan9011 <redwan390309@gmail.com>
2024-07-03 12:46:12 +0600

    set up basic react file and create navbar

---
+++ src/Page/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Navabar from "../../Components/Navbar/Navabar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <div className="max-w-7xl mx-auto">
+            <Navabar></Navabar>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
